6c1dce636afbe98ad2dabe3ea6cf9a991628071f
xihuiliu <liuxihui168168@163.com>
2021-04-01 00:34:18 +0800

    P70爬楼梯

---
+++ app/src/main/java/com/lxh/algorithm/P118Generate.java
@@ -0,0 +1,52 @@
+package com.lxh.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+ * <p>
+ * <p>
+ * <p>
+ * 在杨辉三角中，每个数是它左上方和右上方的数的和。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: 5
+ * 输出:
+ * [
+ * [1],
+ * [1,1],
+ * [1,2,1],
+ * [1,3,3,1],
+ * [1,4,6,4,1]
+ * ]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/pascals-triangle
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+public class P118Generate {
+    public static void main(String[] args) {
+        generate(5);
+    }
+
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(res.get(i - 1).get(j - 1) + res.get(i - 1).get(j));
+                }
+            }
+            res.add(row);
+        }
+        return res;
+
+    }
+}
